910c97e4a1bb67f0238321aee10be913513e939d
1spyral <83098914+1spyral@users.noreply.github.com>
2025-01-16 22:01:59 -0500

    Implement custom map markers

--- wwwfrontend/src/components/Map.tsx
+++ wwwfrontend/src/components/Map.tsx
@@ -1,20 +1,43 @@
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { MapContainer, TileLayer, Popup, CircleMarker } from "react-leaflet";
 import { CitiesContext } from "../context/CitiesContext";
 import { useContext } from "react";
+
+function getColor(layerIndex: number, layers: number) {
+	let percentage = layerIndex / (layers - 1);
+	
+	if (Number.isNaN(percentage)) {
+		percentage = 0;
+	}
+	
+	// Calculate the green intensity based on the percentage
+	const red = 255; // Full red intensity
+	const green = Math.round(percentage * 255); // Green intensity increases as percentage increases
+	const blue = 0; // No blue component
+  
+	return `rgb(${red}, ${green}, ${blue})`;
+  }  
 
 function Map() {
 	const { cities } = useContext(CitiesContext);
 
-	const markers = cities.flat().map((city, index) => {
-		const position: [number, number] = [city.coordinates.lat, city.coordinates.long];
-		return <Marker position={position} key={index}>
-			<Popup>
-				{city.city}, {city.country}
-			</Popup>
-		</Marker>
-	})
+	let index = 0;
+
+	const markers = cities.map((layer, layerIndex) => {
+		const color = getColor(layerIndex, cities.length);
+
+		return layer.map(city => {
+			index++;
+			const position: [number, number] = [city.coordinates.lat, city.coordinates.long];
+			return <CircleMarker center={position} key={index} radius={5} weight={1} color={color} fillColor={color} fillOpacity={0.5}>
+				<Popup>
+					{city.city}, {city.country}
+				</Popup>
+			</CircleMarker>
+		})
+	}).flat();
 
 	const center: [number, number] = [0, 0];
+
 	return (
 		<MapContainer
 			center={center}
